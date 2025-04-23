6f38359cf805e7f8c7fbd1718fe3d96c1e94bb1c
chrispy <cbrough000@gmail.com>
2025-04-22 19:34:22 -0600

    ♻️  Refactor Map component to import detailAreas from a new data file
    ref #6

---
+++ frontend/src/assets/data/mapAreas.js
@@ -0,0 +1,14 @@
+export const detailAreas = [
+  {
+    id: 'A1',
+    baseSquare: 'A1',
+    position: { x: 116, y: 113 },
+    size: { width: 69, height: 69 },
+    imagePrefix: 'A1',
+    resolutions: {
+      medium: { width: 69, height: 69 },
+      detailed: { width: 680, height: 680 }
+    }
+  }
+  // TODO: Add da rest
+];
